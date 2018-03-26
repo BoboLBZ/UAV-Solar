1fcb405af8f1f0fac0514e6c93d4b449055e2b94
BoboLBZ <boboliu1996@gmail.com>
2018-03-26 21:27:54 +0800

    database to record mission

--- app/src/main/java/com/hitices/autopatrol/fragment/MediaSDFragment.java
+++ app/src/main/java/com/hitices/autopatrol/fragment/MediaSDFragment.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,7 @@ import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.hitices.autopatrol.AutoPatrolApplication;
 import com.hitices.autopatrol.R;
 import com.hitices.autopatrol.activity.PatrolMainActivity;
+import com.hitices.autopatrol.helper.DJIMediaHelper;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -41,71 +43,10 @@ public class MediaSDFragment extends Fragment {
     private static final String TAG = PatrolMainActivity.class.getName();
     private OnFragmentInteractionListener mListener;
 
-    private RecyclerView listView;
-    private List<MediaFile> mediaFileList = new ArrayList<MediaFile>();
-    private FileListAdapter mListAdapter;
-    private MediaManager mMediaManager;
-    private int lastClickViewIndex = -1;
-    private View lastClickView;
-    private MediaManager.FileListState currentFileListState = MediaManager.FileListState.UNKNOWN;
-    private FetchMediaTaskScheduler scheduler;
-
+    private GridView gridView;
     private FloatingActionButton refresh;
-    private FetchMediaTask.Callback taskCallback = new FetchMediaTask.Callback() {
-        @Override
-        public void onUpdate(MediaFile file, FetchMediaTaskContent option, DJIError error) {
-            if (null == error) {
-                if (option == FetchMediaTaskContent.PREVIEW) {
-                    mListAdapter.notifyDataSetChanged();
-                }
-                if (option == FetchMediaTaskContent.THUMBNAIL) {
-                    mListAdapter.notifyDataSetChanged();
-                }
-            } else {
-                DJILog.e(TAG, "Fetch Media Task Failed" + error.getDescription());
-            }
-        }
-    };
-    private MediaManager.FileListStateListener updateFileListStateListener = new MediaManager.FileListStateListener() {
-        @Override
-        public void onFileListStateChange(MediaManager.FileListState state) {
-            currentFileListState = state;
-        }
-    };
-    private MediaManager.VideoPlaybackStateListener updatedVideoPlaybackStateListener =
-            new MediaManager.VideoPlaybackStateListener() {
-                @Override
-                public void onUpdate(MediaManager.VideoPlaybackState videoPlaybackState) {
-//                    updateStatusTextView(videoPlaybackState);
-                }
-            };
-    private View.OnClickListener itemViewOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            lastClickViewIndex = (int) (v.getTag());
 
-            if (lastClickView != null && lastClickView != v) {
-                lastClickView.setSelected(false);
-            }
-            v.setSelected(true);
-            lastClickView = v;
-        }
-    };
-    private View.OnClickListener ImgOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            MediaFile selectedMedia = (MediaFile) v.getTag();
-            final Bitmap previewImage = selectedMedia.getPreview();
-            getActivity().runOnUiThread(new Runnable() {
-                public void run() {
-//                    mDisplayImageView.setVisibility(View.VISIBLE);
-//                    mDisplayImageView.setImageBitmap(previewImage);
-                }
-            });
-        }
-    };
-
+    private DJIMediaHelper helper;
     public MediaSDFragment() {
         // Required empty public constructor
     }
@@ -125,7 +66,6 @@ public class MediaSDFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        initMediaManager();
     }
 
     @Override
@@ -133,19 +73,16 @@ public class MediaSDFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the waypoint_preview_waypoint_detail for this fragment
         View view = inflater.inflate(R.layout.fragment_media_sd, container, false);
-        listView = view.findViewById(R.id.imageList);
-        mListAdapter = new FileListAdapter();
-        listView.setAdapter(mListAdapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), OrientationHelper.VERTICAL, false);
-        listView.setLayoutManager(layoutManager);
-
+        gridView = view.findViewById(R.id.sd_imageGrid);
         refresh = view.findViewById(R.id.media_sd_refresh);
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getFileList();
+
             }
         });
+        helper=new DJIMediaHelper(getActivity(),getContext(),taskCallback);
+
         return view;
     }
 
@@ -165,236 +102,29 @@ public class MediaSDFragment extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
     @Override
     public void onDestroyView() {
-        lastClickView = null;
-        if (mMediaManager != null) {
-            mMediaManager.stop(null);
-            mMediaManager.removeFileListStateCallback(this.updateFileListStateListener);
-            mMediaManager.removeMediaUpdatedVideoPlaybackStateListener(updatedVideoPlaybackStateListener);
-            mMediaManager.exitMediaDownloading();
-            if (scheduler != null) {
-                scheduler.removeAllTasks();
-            }
-        }
-        if (AutoPatrolApplication.getCameraInstance() != null) {
-            AutoPatrolApplication.getCameraInstance().setMode(SettingsDefinitions.CameraMode.SHOOT_PHOTO, new CommonCallbacks.CompletionCallback() {
-                @Override
-                public void onResult(DJIError mError) {
-                    if (mError != null) {
-                        setResultToToast("Set Shoot Photo Mode Failed" + mError.getDescription());
-                    }
-                }
-            });
-        }
-        if (mediaFileList != null) {
-            mediaFileList.clear();
-        }
+
         super.onDestroyView();
     }
 
-    private void initMediaManager() {
-        if (AutoPatrolApplication.getProductInstance() == null) {
-            mediaFileList.clear();
-            mListAdapter.notifyDataSetChanged();
-            DJILog.e(TAG, "Product disconnected");
-        } else {
-            if (null != AutoPatrolApplication.getCameraInstance() && AutoPatrolApplication.getCameraInstance().isMediaDownloadModeSupported()) {
-                mMediaManager = AutoPatrolApplication.getCameraInstance().getMediaManager();
-                if (null != mMediaManager) {
-                    mMediaManager.addUpdateFileListStateListener(this.updateFileListStateListener);
-                    mMediaManager.addMediaUpdatedVideoPlaybackStateListener(this.updatedVideoPlaybackStateListener);
-                    AutoPatrolApplication.getCameraInstance().setMode(SettingsDefinitions.CameraMode.MEDIA_DOWNLOAD, new CommonCallbacks.CompletionCallback() {
-                        @Override
-                        public void onResult(DJIError error) {
-                            if (error == null) {
-                                DJILog.e(TAG, "Set cameraMode success");
-//                                showProgressDialog();
-                                getFileList();
-                            } else {
-                                setResultToToast("Set cameraMode failed");
-                            }
-                        }
-                    });
-                    if (mMediaManager.isVideoPlaybackSupported()) {
-                        DJILog.e(TAG, "Camera support video playback!");
-                    } else {
-                        setResultToToast("Camera does not support video playback!");
-                    }
-                    scheduler = mMediaManager.getScheduler();
-                }
+    private FetchMediaTask.Callback taskCallback =new FetchMediaTask.Callback() {
+        @Override
+        public void onUpdate(MediaFile mediaFile, FetchMediaTaskContent fetchMediaTaskContent, DJIError djiError) {
 
-            } else if (null != AutoPatrolApplication.getCameraInstance()
-                    && !AutoPatrolApplication.getCameraInstance().isMediaDownloadModeSupported()) {
-                setResultToToast("Media Download Mode not Supported");
-            }
         }
-    }
+    };
 
-    private void getFileList() {
-        if (AutoPatrolApplication.getCameraInstance() == null) {
-            setResultToToast("this is no camera connect");
-            return;
-        }
-        mMediaManager = AutoPatrolApplication.getCameraInstance().getMediaManager();
-        if (mMediaManager != null) {
-
-            if ((currentFileListState == MediaManager.FileListState.SYNCING) || (currentFileListState == MediaManager.FileListState.DELETING)) {
-                DJILog.e(TAG, "Media Manager is busy.");
-            } else {
-                mMediaManager.refreshFileList(new CommonCallbacks.CompletionCallback() {
-                    @Override
-                    public void onResult(DJIError error) {
-                        if (null == error) {
-//                            hideProgressDialog();
-
-                            //Reset data
-                            if (currentFileListState != MediaManager.FileListState.INCOMPLETE) {
-                                mediaFileList.clear();
-                                lastClickViewIndex = -1;
-                                lastClickView = null;
-                            }
-
-                            mediaFileList = mMediaManager.getFileListSnapshot();
-                            Collections.sort(mediaFileList, new Comparator<MediaFile>() {
-                                @Override
-                                public int compare(MediaFile lhs, MediaFile rhs) {
-                                    if (lhs.getTimeCreated() < rhs.getTimeCreated()) {
-                                        return 1;
-                                    } else if (lhs.getTimeCreated() > rhs.getTimeCreated()) {
-                                        return -1;
-                                    }
-                                    return 0;
-                                }
-                            });
-                            scheduler.resume(new CommonCallbacks.CompletionCallback() {
-                                @Override
-                                public void onResult(DJIError error) {
-                                    if (error == null) {
-                                        getThumbnails();
-                                        getPreviews();
-                                    }
-                                }
-                            });
-                        } else {
-//                            hideProgressDialog();
-                            setResultToToast("Get Media File List Failed:" + error.getDescription());
-                        }
-                    }
-                });
-            }
-        }
-    }
-
-    private void getThumbnails() {
-        if (mediaFileList.size() <= 0) {
-            setResultToToast("No File info for downloading thumbnails");
-            return;
-        }
-        for (int i = 0; i < mediaFileList.size(); i++) {
-            getThumbnailByIndex(i);
-        }
-    }
-
-    private void getPreviews() {
-        if (mediaFileList.size() <= 0) {
-            setResultToToast("No File info for downloading previews");
-            return;
-        }
-        for (int i = 0; i < mediaFileList.size(); i++) {
-            getPreviewByIndex(i);
-        }
-    }
-
-    private void getThumbnailByIndex(final int index) {
-        FetchMediaTask task = new FetchMediaTask(mediaFileList.get(index), FetchMediaTaskContent.THUMBNAIL, taskCallback);
-        scheduler.moveTaskToEnd(task);
-    }
-
-    private void getPreviewByIndex(final int index) {
-        FetchMediaTask task = new FetchMediaTask(mediaFileList.get(index), FetchMediaTaskContent.PREVIEW, taskCallback);
-        scheduler.moveTaskToEnd(task);
-    }
-
-    private void setResultToToast(final String result) {
-        getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                Toast.makeText(getActivity(), result, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
-    }
-
-    private class ItemHolder extends RecyclerView.ViewHolder {
-        ImageView thumbnail_img;
-        TextView file_name;
-        TextView file_type;
-        TextView file_size;
-        TextView file_time;
-
-        public ItemHolder(View itemView) {
-            super(itemView);
-            this.thumbnail_img = itemView.findViewById(R.id.filethumbnail);
-            this.file_name = itemView.findViewById(R.id.filename);
-            this.file_type = itemView.findViewById(R.id.filetype);
-            this.file_size = itemView.findViewById(R.id.fileSize);
-            this.file_time = itemView.findViewById(R.id.filetime);
-        }
-    }
-    private class FileListAdapter extends RecyclerView.Adapter<ItemHolder> {
-        @Override
-        public int getItemCount() {
-            if (mediaFileList != null) {
-                return mediaFileList.size();
-            }
-            return 0;
-        }
-
-        @Override
-        public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.media_info_item, parent, false);
-            return new ItemHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(ItemHolder mItemHolder, final int index) {
-
-            final MediaFile mediaFile = mediaFileList.get(index);
-            if (mediaFile != null) {
-                if (mediaFile.getMediaType() != MediaFile.MediaType.MOV && mediaFile.getMediaType() != MediaFile.MediaType.MP4) {
-                    mItemHolder.file_time.setVisibility(View.GONE);
-                } else {
-                    mItemHolder.file_time.setVisibility(View.VISIBLE);
-                    mItemHolder.file_time.setText(mediaFile.getDurationInSeconds() + " s");
-                }
-                mItemHolder.file_name.setText(mediaFile.getFileName());
-                mItemHolder.file_type.setText(mediaFile.getMediaType().name());
-                mItemHolder.file_size.setText(mediaFile.getFileSize() + " Bytes");
-                mItemHolder.thumbnail_img.setImageBitmap(mediaFile.getThumbnail());
-                mItemHolder.thumbnail_img.setOnClickListener(ImgOnClickListener);
-                mItemHolder.thumbnail_img.setTag(mediaFile);
-                mItemHolder.itemView.setTag(index);
-
-                if (lastClickViewIndex == index) {
-                    mItemHolder.itemView.setSelected(true);
-                } else {
-                    mItemHolder.itemView.setSelected(false);
-                }
-                mItemHolder.itemView.setOnClickListener(itemViewOnClickListener);
-
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/hitices/autopatrol/helper/FlightRecords.java
@@ -0,0 +1,40 @@
+package com.hitices.autopatrol.helper;
+
+import org.litepal.annotation.Column;
+import org.litepal.crud.DataSupport;
+
+import java.util.Date;
+
+/**
+ * Created by Rhys on 2018/3/26.
+ * email: bozliu@outlook.com
+ */
+
+public class FlightRecords extends DataSupport {
+    @Column(unique = true, defaultValue = "unknown")
+    private int id;
+    private String name;
+    private Date startTime;
+    private Date endTime;
+    public String getName(){
+        return name;
+    }
+    public Date getStartTime(){
+        return startTime;
+    }
+    public Date getEndTime(){
+        return endTime;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setStartTime(Date time){
+        this.startTime=time;
+    }
+    public void setEndTime(Date time){
+        this.endTime=time;
+    }
+    public void setId(int i){
+        this.id=i;
+    }
+}
